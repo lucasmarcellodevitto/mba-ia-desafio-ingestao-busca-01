e520cd613c2c94f32d3db4d306c3602932e43c32
lucasmarcellodevitto <lucas_devitto@hotmail.com>
2026-03-18 22:21:14 -0300

    feat: implementing ingest

--- src/ingest.py
+++ src/ingest.py
@@ -1,13 +1,51 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_core.documents import Document
+from langchain_postgres import PGVector
 
 load_dotenv()
 
-PDF_PATH = os.getenv("PDF_PATH")
-
-def ingest_pdf():
-    pass
-
-
 if __name__ == "__main__":
-    ingest_pdf()
+    print("Starting loading process")
+    
+    for k in ("GOOGLE_API_KEY", "DATABASE_URL", "PG_VECTOR_COLLECTION_NAME"):
+        if not os.getenv(k):
+            raise RuntimeError(f"Environment variable {k} is not set! Check your .env file.")
+    
+    pdf_doc = PyPDFLoader(str(Path(__file__).parent / "files/document.pdf")).load()
+
+    splits = RecursiveCharacterTextSplitter(
+        chunk_size=1000, 
+        chunk_overlap=150, add_start_index=False).split_documents(pdf_doc)
+    if not splits:
+        print("Error loading file content into the database. Check your environment variables and ensure the document.pdf"\
+         "file is present in the src/files folder.")
+        raise SystemExit(0)
+    
+    enriched = [
+    Document(
+        page_content=d.page_content,
+        metadata={k: v for k, v in d.metadata.items() if v not in ("", None)}
+    )
+    for d in splits
+    ]    
+
+    ids = [f"doc-{i}" for i in range(len(enriched))]
+
+    embeddings = GoogleGenerativeAIEmbeddings(model=os.getenv("GOOGLE_EMBEDDING_MODEL", "gemini-embedding-001"))
+
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+        connection=os.getenv("DATABASE_URL"),
+        use_jsonb=True,
+        pre_delete_collection=True
+    )
+
+    store.add_documents(documents=enriched, ids=ids)
+    print("Ending loading process")
